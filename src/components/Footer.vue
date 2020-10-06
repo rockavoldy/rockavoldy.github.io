e69e024506a389dee8eaf92879451b54835da94c
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-10-06 14:59:27 +0700

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,17 @@
 <template>
   <footer
-    class="w-full bg-blue-600 h-6 bottom-0 absolute text-center font-semibold text-xs text-gray-100 py-1"
+    class="w-full bg-blue-600 bottom-0 absolute font-semibold text-xs text-gray-100 py-1 flex justify-center"
   >
-    <span>
-      &copy; 2020 Built with
-      <i class="font-bold text-red-600">&hearts;</i> By
-      <strong class="capitalize">Akhmad Maulana Akbar</strong>
+    <span class="flex">
+      Build with<svg
+        class="mx-1 text-red-600 w-4 h-4 fill-current"
+        viewBox="0 0 24 24"
+      >
+        <path
+          d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z"
+        />
+      </svg>
+      <span>in <strong class="capitalize">Bandung, Indonesia</strong></span>
     </span>
   </footer>
 </template>
