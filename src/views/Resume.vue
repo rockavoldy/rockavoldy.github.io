9a0a335b97aaf26017652d5b4869f71bdcb60cb0
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-10-06 13:24:23 +0700

    change resume data from views to json file

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="resume">
     <h2 class="text-center text-2xl mb-2 uppercase font-semibold">Resume</h2>
-    <div class="container mx-auto w-full md:w-8/12 lg:w-7/12 xl:w-5/12 flex flex-col">
+    <div
+      class="container mx-auto w-full md:w-8/12 lg:w-7/12 xl:w-5/12 flex flex-col"
+    >
       <div class="px-3 md:px-2 mx-auto justify-center flex flex-col">
         <div class="w-auto flex flex-col mb-1">
           <h3 class="font-bold text-xl uppercase mb-3">Education</h3>
-          <div class="mb-2" v-for="(item, index) in education" :key="index">
+          <div class="mb-2" v-for="(item, index) in educations" :key="index">
             <h4 class="uppercase font-semibold">{{ item.school }}</h4>
             <span class="text-sm capitalize">{{ item.date }}</span>
             <ul class="list-disc text-sm list-inside font-medium">
-              <li v-for="(itemact, indexact) in item.activities" :key="indexact">{{ itemact }}</li>
+              <li
+                v-for="(itemact, indexact) in item.activities"
+                :key="indexact"
+              >
+                {{ itemact }}
+              </li>
             </ul>
           </div>
         </div>
@@ -21,7 +28,9 @@
             <h4 class="uppercase font-semibold">{{ item.title }}</h4>
             <span class="text-sm capitalize">{{ item.date }}</span>
             <ul class="list-disc text-sm list-inside font-medium">
-              <li v-for="(itemlist, indexlist) in item.list" :key="indexlist">{{ itemlist }}</li>
+              <li v-for="(itemlist, indexlist) in item.list" :key="indexlist">
+                {{ itemlist }}
+              </li>
             </ul>
           </div>
         </div>
@@ -29,7 +38,11 @@
 
         <div class="w-auto flex flex-col mb-1">
           <h3 class="font-bold text-xl uppercase mb-3">Skill</h3>
-          <div class="mb-2 flex flex-col" v-for="(item, index) in skills" :key="index">
+          <div
+            class="mb-2 flex flex-col"
+            v-for="(item, index) in skills"
+            :key="index"
+          >
             <h6 class="uppercase font-bold text-sm">{{ item.title }}</h6>
             <ul
               class="list-none text-sm list-inside"
@@ -46,46 +59,15 @@
 </template>
 
 <script>
+import { educations, awards, skills } from "@/assets/data/resume.json";
 export default {
   data() {
     return {
-      education: [
-        {
-          school: "Universitas Pendidikan Indonesia",
-          date: "September 2016 - Present",
-          activities: [
-            "Assistant Practicum Pemrograman Internet Courses (January 2019 - June 2019)",
-            "Database lab Assistant (January 2018 - June 2018)"
-          ]
-        },
-        {
-          school: "Vocational High School 2 Garut",
-          date: "July 2013 - February 2016",
-          activities: []
-        }
-      ],
-      awards: [
-        {
-          title: "Finalist Hackathon PANDI 10",
-          date: "24 April 2019",
-          list: [
-            "This Hackathon organized by PANDI for 10th year celebration.",
-            "This Hackatahon intended to use u.id on internet-based services."
-          ]
-        }
-      ],
-      skills: [
-        {
-          title: "Intermediate",
-          list: ["PHP", "Codeigniter 3", "JavaScript", "Vue", "Linux", "Git"]
-        },
-        {
-          title: "Basic",
-          list: ["Laravel", "Nuxt", "Nodejs"]
-        }
-      ]
+      educations: educations,
+      awards: awards,
+      skills: skills,
     };
-  }
+  },
 };
 </script>
 
