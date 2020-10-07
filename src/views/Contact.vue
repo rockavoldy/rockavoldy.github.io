a79204991576d13c35c7dd30ce722d911f69b61c
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-10-07 09:33:33 +0700

    add wording to contact, and linkedin profile

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,27 +1,51 @@
 <template>
   <div class="contact">
     <h2 class="text-center text-2xl mb-2 uppercase font-semibold">Contact</h2>
-    <div class="container mx-auto w-full md:w-8/12 lg:w-7/12 xl:w-5/12 flex flex-col">
+    <div
+      class="container mx-auto w-full md:w-8/12 lg:w-7/12 xl:w-5/12 flex flex-col"
+    >
       <div class="mx-auto justify-center flex flex-col text-gray-800 text-lg">
-        <span class="flex my-1">
-          <i class="mr-1">
-            <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
-              <path
-                d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z"
-              />
-            </svg>
-          </i>Akhmad98[at]aol.com
-        </span>
-        <span class="flex my-1">
-          <i class="mr-1">
-            <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
-              <path
-                d="M9.78,18.65L10.06,14.42L17.74,7.5C18.08,7.19 17.67,7.04 17.22,7.31L7.74,13.3L3.64,12C2.76,11.75 2.75,11.14 3.84,10.7L19.81,4.54C20.54,4.21 21.24,4.72 20.96,5.84L18.24,18.65C18.05,19.56 17.5,19.78 16.74,19.36L12.6,16.3L10.61,18.23C10.38,18.46 10.19,18.65 9.78,18.65Z"
-              />
-            </svg>
-          </i>
-          <a href="https://t.me/rockavoldy" target="_blank">t.me/rockavoldy</a>
-        </span>
+        <div class="mt-1 mb-2">
+          <span class="font-medium text-base">You can find me on</span>
+          <span class="flex">
+            <i class="mr-1">
+              <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
+                <path
+                  d="M19 3A2 2 0 0 1 21 5V19A2 2 0 0 1 19 21H5A2 2 0 0 1 3 19V5A2 2 0 0 1 5 3H19M18.5 18.5V13.2A3.26 3.26 0 0 0 15.24 9.94C14.39 9.94 13.4 10.46 12.92 11.24V10.13H10.13V18.5H12.92V13.57C12.92 12.8 13.54 12.17 14.31 12.17A1.4 1.4 0 0 1 15.71 13.57V18.5H18.5M6.88 8.56A1.68 1.68 0 0 0 8.56 6.88C8.56 5.95 7.81 5.19 6.88 5.19A1.69 1.69 0 0 0 5.19 6.88C5.19 7.81 5.95 8.56 6.88 8.56M8.27 18.5V10.13H5.5V18.5H8.27Z"
+                />
+              </svg> </i
+            ><a href="https://www.linkedin.com/in/akhmad98" target="_blank"
+              >https://www.linkedin.com/in/akhmad98</a
+            >
+          </span>
+        </div>
+        <div class="mt-1 mb-2">
+          <span class="font-medium text-base">or chat me on</span>
+          <span class="flex">
+            <i class="mr-1">
+              <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
+                <path
+                  d="M9.78,18.65L10.06,14.42L17.74,7.5C18.08,7.19 17.67,7.04 17.22,7.31L7.74,13.3L3.64,12C2.76,11.75 2.75,11.14 3.84,10.7L19.81,4.54C20.54,4.21 21.24,4.72 20.96,5.84L18.24,18.65C18.05,19.56 17.5,19.78 16.74,19.36L12.6,16.3L10.61,18.23C10.38,18.46 10.19,18.65 9.78,18.65Z"
+                />
+              </svg>
+            </i>
+            <a href="https://t.me/rockavoldy" target="_blank"
+              >t.me/rockavoldy</a
+            >
+          </span>
+        </div>
+        <div class="mt-1 mb-2">
+          <span class="font-medium text-base">or email me at</span>
+          <span class="flex">
+            <i class="mr-1">
+              <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
+                <path
+                  d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z"
+                />
+              </svg> </i
+            >Akhmad98[at]aol.com
+          </span>
+        </div>
       </div>
     </div>
   </div>
