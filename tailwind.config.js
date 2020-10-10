b8adeae25db086b6d532ff6888e98fe1f2d29f61
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-10-10 17:38:50 +0700

    set tailwind for future and disable tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: false,
   prefix: "",
   important: false,
   separator: ":",
